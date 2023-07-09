c13e0bf39aadaf27fbc353c72d8fe99cda3dd8d4
Abhay Mathur <66543532+abhayma1000@users.noreply.github.com>
2023-07-08 21:04:36 -0400

    Initialized board with static pieces

--- src/game_manager.py
+++ src/game_manager.py
@@ -24,13 +24,51 @@ class Piece(): # piece_team TRUE = WHITE
 class Pawn(Piece):
     def __init__(self, piece_team: bool, piece_square):
         if piece_team:
-            Piece.__init__(self, 5, "Pawn", piece_team, piece_square, "")
+            Piece.__init__(self, 1, "Pawn", piece_team, piece_square, "src/game_pieces/white_pawn.png")
         else:
-            Piece.__init__(self, 5, "Pawn", piece_team, piece_square, "TODO other path")
+            Piece.__init__(self, 1, "Pawn", piece_team, piece_square, "src/game_pieces/black_pawn.png")
     def __str__(self):
         return "Pawn:" + str(self.square) + ", Team:" + str(self.team)
-
-
+class Rook(Piece):
+    def __init__(self, piece_team: bool, piece_square):
+        if piece_team:
+            Piece.__init__(self, 5, "Rook", piece_team, piece_square, "src/game_pieces/white_rook.png")
+        else:
+            Piece.__init__(self, 5, "Rook", piece_team, piece_square, "src/game_pieces/black_rook.png")
+    def __str__(self):
+        return "Rook:" + str(self.square) + ", Team:" + str(self.team)
+class Knight(Piece):
+    def __init__(self, piece_team: bool, piece_square):
+        if piece_team:
+            Piece.__init__(self, 3, "Knight", piece_team, piece_square, "src/game_pieces/white_knight.png")
+        else:
+            Piece.__init__(self, 3, "Knight", piece_team, piece_square, "src/game_pieces/black_knight.png")
+    def __str__(self):
+        return "Knight:" + str(self.square) + ", Team:" + str(self.team)
+class Bishop(Piece):
+    def __init__(self, piece_team: bool, piece_square):
+        if piece_team:
+            Piece.__init__(self, 3, "Bishop", piece_team, piece_square, "src/game_pieces/white_bishop.png")
+        else:
+            Piece.__init__(self, 3, "Bishop", piece_team, piece_square, "src/game_pieces/black_bishop.png")
+    def __str__(self):
+        return "Bishop:" + str(self.square) + ", Team:" + str(self.team)
+class King(Piece):
+    def __init__(self, piece_team: bool, piece_square):
+        if piece_team:
+            Piece.__init__(self, 0, "King", piece_team, piece_square, "src/game_pieces/white_king.png")
+        else:
+            Piece.__init__(self, 0, "King", piece_team, piece_square, "src/game_pieces/black_king.png")
+    def __str__(self):
+        return "King:" + str(self.square) + ", Team:" + str(self.team)
+class Queen(Piece):
+    def __init__(self, piece_team: bool, piece_square):
+        if piece_team:
+            Piece.__init__(self, 9, "Queen", piece_team, piece_square, "src/game_pieces/white_queen.png")
+        else:
+            Piece.__init__(self, 9, "Queen", piece_team, piece_square, "src/game_pieces/black_queen.png")
+    def __str__(self):
+        return "Queen:" + str(self.square) + ", Team:" + str(self.team)
 
 
 
@@ -39,11 +77,24 @@ class GameManager():
         # (0, 0) is top left
         self.board = np.empty((8, 8), dtype=Piece)
 
+        side = False
+        for i in [0, 7]:
+            
+            self.board[i][0] = Rook(side, (i, 0))
+            self.board[i][1] = Knight(side, (i, 1))
+            self.board[i][2] = Bishop(side, (i, 2))
+            self.board[i][3] = Queen(side, (i, 3))
+            self.board[i][4] = King(side, (i, 4))
+            self.board[i][5] = Bishop(side, (i, 5))
+            self.board[i][6] = Knight(side, (i, 6))
+            self.board[i][7] = Rook(side, (i, 7))
+
+            side = True
+
         for i in range(8):
-            self.board[0][i] = Pawn(False, (0, i))
+            self.board[1][i] = Pawn(False, (0, i))
+            self.board[6][i] = Pawn(True, (6, i))
 
-
-        # TODO fill-in the board
     def __str__(self):
         return "The game manager"
     def get_board(self):
--- src/graphics_manager.py
+++ src/graphics_manager.py
@@ -2,10 +2,13 @@ import tkinter as tk
 import numpy as np
 import tkmacosx
 from tkinter import ttk
+from tkinter import *
+from PIL import Image, ImageTk
 import math
 
+
 class GraphicsManager():
-    def __init__(self):
+    def __init__(self, board):
         self.window_size = [1400, 800]
         self.window_ratio = self.window_size[0] / self.window_size[1]
         
@@ -16,15 +19,14 @@ class GraphicsManager():
         window.resizable(width=False, height=False)
 
         board_frame = tk.Frame(window)
-        self.buttons = self.initialize_board(board_frame)
+        self.buttons = self.initialize_board(board_frame, board)
         board_frame.pack(pady=20)
 
-
-        # window.mainloop()
-
+        window.mainloop()
 
         return
-    def initialize_board(self, window):
+
+    def initialize_board(self, window, board):
         square_size = 90
         counter = 1
 
@@ -45,24 +47,17 @@ class GraphicsManager():
                 if counter % 2 == 0:
                     color = "white"
 
-                button = tkmacosx.Button(master=frame, bg=color, highlightbackground="black", height=square_size, width = square_size)
+                if board[i][j] != None:
+                    image1 = PhotoImage(file=board[i][j].image_path)
+                    button = tkmacosx.Button(master=frame, bg=color, highlightbackground="black", height=square_size, width = square_size, image=image1)
+                else:
+                    button = tkmacosx.Button(master=frame, bg=color, highlightbackground="black", height=square_size, width = square_size)
                 button.pack()
                 buttons[i][j] = button
 
                 counter += 1
         
         return buttons
-        
-        # label2 = tk.Label(master=frame, text=str(self.window_ratio)).pack()
-    def populate_board(self, window, board):
-        for i in range(8):
-            for j in range(8):
-                # self.buttons[i][j] SOMEOPERATION --> board[i][j].image_path
-                # Want the image_path to be placed on top of that button
-                return None
-        
-
-                # TODO how do I connect board to actual board
 
     def __str__(self):
         return "The graphics"
